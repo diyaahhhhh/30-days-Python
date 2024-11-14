1e97b051a64054fac3221af4ababc8ef253d0ed9
NoelShaji001 <noelshaji257202@gmail.com>
2024-11-14 21:06:02 +0530

    Update Noel Shaji.py

--- README.md/Day3/Noel Shaji.py
+++ README.md/Day3/Noel Shaji.py
@@ -1,1 +1,15 @@
+'''
+Author:Noel Shaji
+Date:14-11-2024
+'''
+a=float(input("Enter a number: "))
+b=float(input("Enter a number: "))
+sum=a+b
+difference=a-b
+multiplication=a*b
+division=a/b
+print(f"The sum of {a} and {b} is {sum}
+\nThe difference when {b} is substracted from {a} is {difference}
+\nThe product of {a} and {b} is {multiplication}
+\nThe quotient when {a} divided by {b} is {division}.")
 
