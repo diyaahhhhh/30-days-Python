d5aeeaf7625201f064a3a621e28ce8bfb0ba7e35
NoelShaji001 <noelshaji257202@gmail.com>
2024-11-15 21:19:06 +0530

    Update Noel Shaji.py

--- README.md/Day4/Noel Shaji.py
+++ README.md/Day4/Noel Shaji.py
@@ -1,1 +1,29 @@
+'''
+Author:Noel Shaji
+Date:15-11-2024
+'''
 
+#Task 1
+
+num=int(input("Enter a number: "))
+if num>0:
+	print("The number is positive")
+elif num<0:
+	print("The number is negative")
+else:
+	print("The number is 0")
+	
+#Task 2
+
+age=int(input("Enter your age: "))
+if age<13:
+	print("You are a child")
+elif age>=13 and age<20:
+	print("You are a teenager")
+elif age>=20 and age<70:
+	print("You are an adult")
+else:
+	print("you are an old person")
+	
+
+	
