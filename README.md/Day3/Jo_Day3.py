2a8df603ab16e9d6ef5395f1c4d07000ae94ab64
Joe Joseph <joejoseph2028@cs.sjcetpalai.ac.in>
2024-11-16 18:52:15 +0530

    Add files via upload

---
+++ README.md/Day3/Jo_Day3.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter a number: "))
+num2=int(input("Enter another number: "))
+
+add=num1+num2
+print(add)
+diff=num1-num2
+print(diff)
+product=num1*num2
+print(product)
+div=num1/num2
+print(div)
