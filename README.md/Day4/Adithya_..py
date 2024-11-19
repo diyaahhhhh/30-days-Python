fce15ccefdb918f34cd6d8dff5a7b45d57db085b
Adithyas0710 <adithyas1064@gmail.com>
2024-11-19 21:02:31 +0530

    Add files via upload

---
+++ README.md/Day4/Adithya_..py
@@ -0,0 +1,20 @@
+# Task-1
+
+number = int(input("Enter a number:"))
+if number>0:
+    print("The number is positive")
+elif number<0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+
+# Task-2
+
+person_age = int(input("Enter your age"))
+if 20<=person_age<=59:
+    print("The person is adult")
+elif 13<=person_age<=19:
+    print("The person is teenager")
+else:
+    print("The person is a child")
+
