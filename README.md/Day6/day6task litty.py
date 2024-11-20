0e43c5a92eb2d918b3ea79762458f42ef325951b
litty12 <littyjomon2028@cs.sjcetpalai.ac.in>
2024-11-20 22:10:57 +0530

    Add files via upload

---
+++ README.md/Day6/day6task litty.py
@@ -0,0 +1,13 @@
+'''
+Author:Litty jomon
+description:list creation
+'''
+fruits=['Apple','Banana','cherry']
+fruits.append('orange')
+fruits.remove('Banana')
+print(fruits[2])
+print(fruits)
+
+#BONUS
+fruits.sort()
+print(fruits)
