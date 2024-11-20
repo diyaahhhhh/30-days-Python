622a2999418f46b95209bf031729d919d603eb56
litty12 <littyjomon2028@cs.sjcetpalai.ac.in>
2024-11-20 21:59:18 +0530

    Add files via upload

---
+++ README.md/Day5/day5 litty.py
@@ -0,0 +1,9 @@
+'''
+Author:Litty
+description:to check number is even or odd
+'''
+number=int(input("Enter a number"))
+if number%2==0:
+    print("Number is even")
+else:
+    print("The number is odd")
