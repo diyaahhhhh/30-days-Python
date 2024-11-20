c07878037f310132132a3c2c582ad6677a3e6932
litty12 <littyjomon2028@cs.sjcetpalai.ac.in>
2024-11-20 21:34:55 +0530

    Add files via upload

---
+++ README.md/Day3/day3 litty.py
@@ -0,0 +1,14 @@
+'''
+Author:Litty jomon
+description:mathematical operations
+'''
+a=5
+b=6
+Sum= a + b
+print(Sum)
+Difference = a - b
+print(Difference)
+Product = a * b
+print(Product)
+Quotient = a / b
+print(Quotient)
