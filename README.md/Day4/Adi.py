14dfb2e445c412343a74bbbeaf8e043846fc7e65
Adi <adithyannp80@gmail.com>
2024-11-20 20:49:22 +0530

    Add files via upload

---
+++ README.md/Day4/Adi.py
@@ -0,0 +1,16 @@
+'''
+name:adi
+date:14/11/2024
+'''
+
+
+a=float(input("Enter a number: "))
+b=float(input("Enter a number: "))
+sum=a+b
+difference=a-b
+product=a*b
+division=a/b
+print((f"The sum of {a} and {b} is {sum}"
+       f"\nThe difference of {b} and {a} is {difference}"
+       f"\nThe product of {a} and {b} is {product}"
+       f"\nThe quotient when {a} divided by {b} is {division}."))
